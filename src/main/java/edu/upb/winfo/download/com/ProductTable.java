7289d33f05c4cf66f719670f2adbe7777619b89c
Sebastian Klatte <sebastianklatte@gmx.net>
2015-12-31 16:42:38 +0100

    Improved database table classes.

--- src/main/java/edu/upb/winfo/download/com/ProductTable.java
+++ src/main/java/edu/upb/winfo/download/com/ProductTable.java
@@ -20,22 +20,27 @@ public class ProductTable {
 	}
 
 	public void updateProduct(int pid, int oid, String editors_review_name, Date editors_review_date,
-			String editors_review_description, String editors_review_text, int editors_review_rating,
-			int users_review_rating, int users_review_rating_count, String publisher_description, String publisher_name,
-			String publisher_url, Date publish_date, String platform, String category, String subcategory,
-			int latest_id_v) throws SQLException {
+	                          String editors_review_description, String editors_review_text,
+	                          double editors_review_rating, double users_review_rating,
+	                          int users_review_rating_count, String publisher_description, String publisher_name,
+	                          String publisher_url, String platform, String category,
+	                          String subcategory, int latest_id_v) throws SQLException {
 		Statement stmt = null;
 		try {
 			stmt = con.createStatement();
 			if (!stmt.execute("SELECT pid FROM download_com_products " + "WHERE pid = '" + pid + "'")) {
 				stmt.executeUpdate(
-						"INSERT INTO `download_com_products`(`pid`, `oid`, `editors_review_name`, `editors_review_date`, `editors_review_description`, `editors_review_text`, `editors_review_rating`, `users_review_rating`, `users_review_rating_count`, `publisher_description`, `publisher_name`, `publisher_url`, `publish_date`, `platform`, `category`, `subcategory`, `latest_id_v`) "
+						"INSERT INTO `download_com_products`(`pid`, `oid`, `editors_review_name`, "
+								+ "`editors_review_date`, `editors_review_description`, `editors_review_text`, "
+								+ "`editors_review_rating`, `users_review_rating`, `users_review_rating_count`, "
+								+ "`publisher_description`, `publisher_name`, `publisher_url`, `platform`, "
+								+ "`category`, `subcategory`, `latest_id_v`) "
 								+ "VALUES('" + pid + "', '" + oid + "', '" + editors_review_name + "', '"
 								+ editors_review_date + "', '" + editors_review_description + "', '"
 								+ editors_review_text + "', '" + editors_review_rating + "', '" + users_review_rating
 								+ "', '" + users_review_rating_count + "', '" + publisher_description + "', '"
-								+ publisher_name + "', '" + publisher_url + "', '" + publish_date + "', '" + platform
-								+ "', '" + category + "', '" + subcategory + "', '" + latest_id_v + ")");
+								+ publisher_name + "', '" + publisher_url + "', '" + platform
+								+ "', '" + category + "', '" + subcategory + "', '" + latest_id_v + "')");
 			} else {
 				stmt.executeUpdate("UPDATE `download_com_products` SET `oid`='" + oid + "',`editors_review_name`='"
 						+ editors_review_name + "',`editors_review_date`='" + editors_review_date
@@ -44,9 +49,8 @@ public class ProductTable {
 						+ "',`users_review_rating`='" + users_review_rating + "',`users_review_rating_count`='"
 						+ users_review_rating_count + "',`publisher_description`='" + publisher_description
 						+ "',`publisher_name`='" + publisher_name + "',`publisher_url`='" + publisher_url
-						+ "',`publish_date`='" + publish_date + "',`platform`='" + platform + "',`category`='"
-						+ category + "',`subcategory`='" + subcategory + "',`latest_id_v`='" + latest_id_v
-						+ "' WHERE pid = '" + pid + "'");
+						+ "',`platform`='" + platform + "',`category`='" + category + "',`subcategory`='"
+						+ subcategory + "',`latest_id_v`='" + latest_id_v + "' WHERE pid = '" + pid + "'");
 			}
 
 		} catch (SQLException e) {
--- src/main/java/edu/upb/winfo/download/com/ProductVersionTable.java
+++ src/main/java/edu/upb/winfo/download/com/ProductVersionTable.java
@@ -8,54 +8,61 @@ import java.sql.Statement;
 
 public class ProductVersionTable {
 
-	private String dbName;
-	private Connection con;
-	private String dbms;
+    private String dbName;
+    private Connection con;
+    private String dbms;
 
-	public ProductVersionTable(Connection connArg, String dbNameArg, String dbmsArg) {
-		super();
-		this.con = connArg;
-		this.dbName = dbNameArg;
-		this.dbms = dbmsArg;
-	}
+    public ProductVersionTable(Connection connArg, String dbNameArg, String dbmsArg) {
+        super();
+        this.con = connArg;
+        this.dbName = dbNameArg;
+        this.dbms = dbmsArg;
+    }
 
-	public void updateProductVersion(int id_p, int vid, String version_name, String version_alterations,
-			Date version_date, String version_identifier, String operating_systems, String additional_requirements,
-			String download_size, String download_name, String download_link, int downloads_total,
-			int downloads_last_week, String license_model, String license_limitations, String license_cost)
-					throws SQLException {
-		Statement stmt = null;
-		try {
-			stmt = con.createStatement();
-			if (!stmt.execute("SELECT vid FROM download_com_product_versions " + "WHERE id_p = '" + id_p
-					+ "' AND vid = '" + vid + "'")) {
-				stmt.executeUpdate(
-						"INSERT INTO `download_com_product_versions`(`id_p`, `vid`, `version_name`, `version_alterations`, `version_date`, `version_identifier`, `operating_systems`, `additional_requirements`, `download_size`, `download_name`, `download_link`, `downloads_total`, `downloads_last_week`, `license_model`, `license_limitations`, `license_cost`) "
-								+ "VALUES('" + id_p + "', '" + vid + "', '" + version_name + "', '"
-								+ version_alterations + "', '" + version_date + "', '" + version_identifier + "', '"
-								+ operating_systems + "', '" + additional_requirements + "', '" + download_size + "', '"
-								+ download_name + "', '" + download_link + "', '" + downloads_total + "', '"
-								+ downloads_last_week + "', '" + license_model + "', '" + license_limitations + "', '"
-								+ license_cost + "')");
-			} else {
-				stmt.executeUpdate("UPDATE `download_com_product_versions` SET `version_name`='" + version_name
-						+ "',`version_alterations`='" + version_alterations + "',`version_date`='" + version_date
-						+ "',`version_identifier`='" + version_identifier + "',`operating_systems`='"
-						+ operating_systems + "',`additional_requirements`='" + additional_requirements
-						+ "',`download_size`='" + download_size + "',`download_name`='" + download_name
-						+ "',`download_link`='" + download_link + "',`downloads_total`='" + downloads_total
-						+ "',`downloads_last_week`='" + downloads_last_week + "',`license_model`='" + license_model
-						+ "',`license_limitations`='" + license_limitations + "',`license_cost`='" + license_cost
-						+ "' WHERE id_p = '" + id_p + "' AND vid = '" + vid + "'");
-			}
+    public void updateProductVersion(int id_p, int vid, String version_name, String version_alterations,
+                                     Date version_publish_date, Date version_added_date, String version_identifier,
+                                     String operating_systems, String additional_requirements, String download_size,
+                                     String download_name, String download_link, int downloads_total,
+                                     int downloads_last_week, String license_model, String license_limitations,
+                                     String license_cost)
+            throws SQLException {
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            if (!stmt.execute("SELECT vid FROM download_com_product_versions " + "WHERE id_p = '" + id_p
+                    + "' AND vid = '" + vid + "'")) {
+                stmt.executeUpdate(
+                        "INSERT INTO `download_com_product_versions`(`id_p`, `vid`, `version_name`, "
+                                + "`version_alterations`, `version_publish_date`, `version_added_date`, "
+                                + "`version_identifier`, `operating_systems`, `additional_requirements`, "
+                                + "`download_size`, `download_name`, `download_link`, `downloads_total`, "
+                                + "`downloads_last_week`, `license_model`, `license_limitations`, `license_cost`) "
+                                + "VALUES('" + id_p + "', '" + vid + "', '" + version_name + "', '"
+                                + version_alterations + "', '" + version_publish_date + "', '" + version_added_date
+                                + "', '" + version_identifier + "', '" + operating_systems + "', '"
+                                + additional_requirements + "', '" + download_size + "', '" + download_name + "', '"
+                                + download_link + "', '" + downloads_total + "', '" + downloads_last_week + "', '"
+                                + license_model + "', '" + license_limitations + "', '" + license_cost + "')");
+            } else {
+                stmt.executeUpdate("UPDATE `download_com_product_versions` SET `version_name`='" + version_name
+                        + "',`version_alterations`='" + version_alterations + "',`version_publish_date`='"
+                        + version_publish_date + "',`version_added_date`='" + version_added_date
+                        + "',`version_identifier`='" + version_identifier + "',`operating_systems`='"
+                        + operating_systems + "',`additional_requirements`='" + additional_requirements
+                        + "',`download_size`='" + download_size + "',`download_name`='" + download_name
+                        + "',`download_link`='" + download_link + "',`downloads_total`='" + downloads_total
+                        + "',`downloads_last_week`='" + downloads_last_week + "',`license_model`='" + license_model
+                        + "',`license_limitations`='" + license_limitations + "',`license_cost`='" + license_cost
+                        + "' WHERE id_p = '" + id_p + "' AND vid = '" + vid + "'");
+            }
 
-		} catch (SQLException e) {
-			Database.printSQLException(e);
-		} finally {
-			if (stmt != null) {
-				stmt.close();
-			}
-		}
-	}
+        } catch (SQLException e) {
+            Database.printSQLException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
 
 }
