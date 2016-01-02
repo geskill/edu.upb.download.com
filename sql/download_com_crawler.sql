79ebe382034906e84a0c23e9a2ebd1ca9522f970
Sebastian Klatte <sebastianklatte@gmx.net>
2016-01-03 00:00:03 +0100

    Improved database structure (2).

--- sql/download_com_crawler.sql
+++ sql/download_com_crawler.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 31. Dez 2015 um 16:38
+-- Erstellungszeit: 02. Jan 2016 um 23:56
 -- Server-Version: 5.6.24
 -- PHP-Version: 5.6.8
 
@@ -34,19 +34,20 @@ CREATE TABLE IF NOT EXISTS `download_com_products` (
   `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'internal usage',
   `pid` int(9) NOT NULL COMMENT 'product id',
   `oid` int(9) NOT NULL COMMENT 'ontology id',
-  `editors_review_name` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `editors_review_date` date NOT NULL,
+  `editors_review_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `editors_review_date` date DEFAULT NULL,
   `editors_review_description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `editors_review_text` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `editors_review_rating` double NOT NULL,
   `users_review_rating` double NOT NULL,
   `users_review_rating_count` int(6) NOT NULL,
   `publisher_description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publisher_description_alterations` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `publisher_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `publisher_url` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `platform` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `subcategory` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `platform` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `subcategory` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `latest_id_v` int(9) NOT NULL COMMENT 'latest version of the product'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -58,14 +59,14 @@ CREATE TABLE IF NOT EXISTS `download_com_products` (
 
 CREATE TABLE IF NOT EXISTS `download_com_product_user_reviews` (
   `id` int(11) NOT NULL COMMENT 'internal usage',
-  `created` datetime NOT NULL COMMENT 'internal usage',
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'internal usage',
   `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'internal usage',
   `mid` int(9) NOT NULL COMMENT 'message id',
   `id_p` int(9) NOT NULL COMMENT 'related product id',
   `id_v` int(9) NOT NULL COMMENT 'related version id of product',
   `rating` double NOT NULL,
-  `title` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `author` varchar(35) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date` date NOT NULL,
   `pros` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `cons` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -86,11 +87,11 @@ CREATE TABLE IF NOT EXISTS `download_com_product_versions` (
   `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'internal usage',
   `id_p` int(9) NOT NULL COMMENT 'related product id',
   `vid` int(9) NOT NULL COMMENT 'version id of the product',
-  `version_name` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `version_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `version_alterations` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `version_publish_date` date NOT NULL,
   `version_added_date` date NOT NULL,
-  `version_identifier` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `version_identifier` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `operating_systems` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `additional_requirements` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `download_size` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
